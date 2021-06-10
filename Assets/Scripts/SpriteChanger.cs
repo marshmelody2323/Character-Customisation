fe2f48326767c470467a1238f6874486544acb30
Melody Lynn <mysticcreeper99@gmail.com>
2021-06-10 11:08:46 +1000

    Update 2
    Character customizer is working now but  not all the sprites are in the right places

---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+
+    [Header("Sprite To Change")]
+    public SpriteRenderer bodyPart;
+
+    [Header("Sprites to Cycle Through")]
+    public List<Sprite> options = new List<Sprite>();
+
+    private int currentOption = 0;
+
+    public void NextOption()
+    {
+        currentOption++;
+        if(currentOption >= options.Count)
+        {
+            currentOption = 0; //Makes it so it just resets and cycles through again
+        }
+
+        bodyPart.sprite = options[currentOption];
+    }
+
+    public void PreviousOption()
+    {
+        currentOption--;
+        if(currentOption <= 0)
+        {
+            currentOption = options.Count - 1; //Makes it so it just resets and cycles through again but the other way around
+        }
+
+        bodyPart.sprite = options[currentOption];
+    }
+  
+}
